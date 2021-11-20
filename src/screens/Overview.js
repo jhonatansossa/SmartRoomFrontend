88432da4b09cd88cfbb33568c99d4a82b9cf55bc
Ingo Staut <ingo.staut@stud.h-da.de>
2021-11-20 12:33:51 +0100

    Created 2 horizontal scroll areas with dummy data

--- src/screens/Overview.js
+++ src/screens/Overview.js
@@ -1,10 +1,71 @@
-import React from 'react'
-import { Col, Row } from 'react-bootstrap'
-
+import React from "react";
+import { Col, Row } from "react-bootstrap";
 
 const Overview = () => {
-    return <div> Overview screen </div>
-    
-}
+  return (
+    <>
+      <div>
+        <div
+          style={{
+            height: "240px",
+          }}
+        >
+          Space for smart room overview
+        </div>
 
-export default Overview
+        <div className="header">
+          <div>Turned on</div>
+          <button className="btn-primary-no-background">View all</button>
+        </div>
+
+        <div className="scroll-area">
+          {devices_turnedOn.map((src) => (
+            <div
+              key={src.title}
+              className="card"
+              style={{
+                backgroundImage: `url(${src.image})`,
+              }}
+            >
+              <div className="card-title">{src.title}</div>
+            </div>
+          ))}
+        </div>
+        <div className="header">
+          <div>Frequently used</div>
+          <button className="btn-primary-no-background">View all</button>
+        </div>
+        <div className="scroll-area">
+          {devices_frequentlyUsed.map((src) => (
+            <div
+              key={src.title}
+              className="card"
+              style={{
+                backgroundImage: `url(${src.image})`,
+              }}
+            >
+              <div className="card-title">{src.title}</div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+const devices_turnedOn = [
+  { image: "/logo192.png", title: "device1" },
+  { image: "/logo192.png", title: "device2" },
+  { image: "/logo192.png", title: "device3" },
+  { image: "/logo192.png", title: "device4" },
+  { image: "/logo192.png", title: "device5" },
+  { image: "/logo192.png", title: "device6" },
+];
+
+const devices_frequentlyUsed = [
+  { image: "/logo192.png", title: "device1" },
+  { image: "/logo192.png", title: "device2" },
+  { image: "/logo192.png", title: "device3" },
+];
+
+export default Overview;
