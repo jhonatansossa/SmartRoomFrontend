1154720f851afa2129d3da083ae69a651a3f8fb6
Ingo Staut <ingo.staut@stud.h-da.de>
2021-12-06 14:44:22 +0100

    Remove unused Login.js file

--- src/screens/Login/Login.js
+++ src/screens/Login/Login.js
@@ -1,13 +1,12 @@
 import { React, useState } from "react";
 import { Col, Row } from "react-bootstrap";
 import { useNavigate, generatePath } from "react-router-dom";
-import user from "./users";
+import user from "./Users";
 
 const Login = () => {
   var [usernameValue, setUsernameValue] = useState();
-  var [passwordValue, setPasswordValue] = useState();  
+  var [passwordValue, setPasswordValue] = useState();
   let navigate = useNavigate();
-
 
   // Initialize a boolean state
   const [passwordShown, setPasswordShown] = useState(false);
@@ -21,28 +20,30 @@ const Login = () => {
 
   const authenticateUser = (usernameValue, passwordValue) => {
     let isCorrect = false;
-    user.login.map((eachUser) =>{
-      if(usernameValue === eachUser.username && passwordValue === eachUser.password){
+    user.login.map((eachUser) => {
+      if (
+        usernameValue === eachUser.username &&
+        passwordValue === eachUser.password
+      ) {
         isCorrect = true;
         // setCookies(eachUser.name);
         navigate("/overview");
       }
     });
-    if(!isCorrect)
-      alert("Wrong username or password, please try again");
-  }
+    if (!isCorrect) alert("Wrong username or password, please try again");
+  };
 
   return (
     <div className="login-container">
       <h1 className="huge-header">Log in</h1>
-      <form name="login-form" >
+      <form name="login-form">
         <input
           id="username"
           placeholder="Username"
           className="form-control form-control-input"
           required
           value={usernameValue}
-          onChange={e => setUsernameValue(e.target.value)}
+          onChange={(e) => setUsernameValue(e.target.value)}
         />
         <input
           type={passwordShown ? "text" : "password"}
@@ -51,7 +52,7 @@ const Login = () => {
           className="form-control form-control-input"
           required
           value={passwordValue}
-          onChange={e => setPasswordValue(e.target.value)}
+          onChange={(e) => setPasswordValue(e.target.value)}
         />
         <label className="form-control form-control-input-password">
           <input
@@ -61,11 +62,11 @@ const Login = () => {
           />
           Show password
         </label>
-        <button type="submit" className="form-control btn-primary"
-          onClick={() =>
-            authenticateUser(usernameValue, passwordValue)
-          }
-          >
+        <button
+          type="submit"
+          className="form-control btn-primary"
+          onClick={() => authenticateUser(usernameValue, passwordValue)}
+        >
           Log in
         </button>
       </form>
