f1cd16af159c1e09aec40ccec2a9bc8f734a8d13
Stelios Papapetrou <stelios.papapetrou@scigentech.com>
2021-12-12 21:00:54 +0000

    Dynamic device list

--- src/screens/AllDevices.js
+++ src/screens/AllDevices.js
@@ -1,49 +1,76 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Col, Row } from "react-bootstrap";
 import { useNavigate, generatePath } from "react-router-dom";
 
 const AllDevices = () => {
   let navigate = useNavigate();
 
+  const [error, setError] = React.useState(null);
+  const [isLoaded, setIsLoaded] = React.useState(false);
+  const [devices, setDevices] = React.useState([]);
+
+  // Note: the empty deps array [] means
+  // this useEffect will run once
+  // similar to componentDidMount()
+  React.useEffect(() => {
+    fetch("https://6a3619a8-5eb6-49fe-8061-829b4d1de332.mock.pstmn.io/allDevices")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setDevices(result.devices);
+          console.log(result)
+          console.log(result.lastMeasurements)
+          console.log(result.device)
+        },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      )
+  }, [])
+  console.log(devices)
+
   function redirectToDetailed(id) {
     let path = generatePath("/details/:id", { id });
     navigate(path);
   }
+  
 
   return (
     <div className="vertical-scroll-area">
       {devices.map((src) => (
         <button
           className="card hov-primary vertical"
-          onClick={() => redirectToDetailed(src.title)}
+          onClick={() => redirectToDetailed(src.name)}
         >
-          <div
+          {/* <div
             key={src.title}
             className="card-image vertical"
             style={{
               backgroundImage: `url(${src.image})`,
             }}
-          ></div>
-          <div className="card-title vertical">{src.title}</div>
+          ></div> */}
+          <div className="card-title vertical">{src.name}</div>
         </button>
       ))}
     </div>
   );
 };
 
-const devices = [
-  { title: "device1", image: "/logo192.png" },
-  { title: "device2", image: "/logo192.png" },
-  { title: "device3", image: "/logo192.png" },
-  { title: "device4", image: "/logo192.png" },
-  { title: "device5", image: "/logo192.png" },
-  { title: "device6", image: "/logo192.png" },
-  { title: "device7", image: "/logo192.png" },
-  { title: "device8", image: "/logo192.png" },
-  { title: "device9", image: "/logo192.png" },
-  { title: "device10", image: "/logo192.png" },
-  { title: "device11", image: "/logo192.png" },
-  { title: "device12", image: "/logo192.png" },
-];
+// const devices = [
+//   { title: "device1", image: "/logo192.png" },
+//   { title: "device2", image: "/logo192.png" },
+//   { title: "device3", image: "/logo192.png" },
+//   { title: "device4", image: "/logo192.png" },
+//   { title: "device5", image: "/logo192.png" },
+//   { title: "device6", image: "/logo192.png" },
+//   { title: "device7", image: "/logo192.png" },
+//   { title: "device8", image: "/logo192.png" },
+//   { title: "device9", image: "/logo192.png" },
+//   { title: "device10", image: "/logo192.png" },
+//   { title: "device11", image: "/logo192.png" },
+//   { title: "device12", image: "/logo192.png" },
+// ];
 
 export default AllDevices;
