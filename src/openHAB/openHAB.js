9781da119bc9cb34dcf06948a8e4a9d99efda4f7
Aleksandr Arshavskiy <aleksandrarshavskiy@gmail.com>
2021-12-16 18:28:08 +0100

    renamed folder for openHAB constants, changed REST request so we get only switches we can control

---
+++ src/openHAB/openHAB.js
@@ -0,0 +1,7 @@
+const openHAB = {
+    token:
+        "Bearer oh.test2.LToFbTvvZffhDf1vqHadYtXSWnVVOZYJG7z8Uyz7uVI7EpQgE8udHBCPOvi3z120JSvgXhUcXllA61oDub3A",
+    url: "http://localhost:8080",
+};
+
+module.exports = openHAB;
--- src/screens/Overview.js
+++ src/screens/Overview.js
@@ -1,226 +1,227 @@
-import React, { useEffect, useState } from "react";
+import React, {useEffect, useState} from "react";
 import Axios from "axios";
-import { generatePath, useNavigate } from "react-router-dom";
-import openHAB from "../Token/openHAB";
+import {generatePath, useNavigate} from "react-router-dom";
+import openHAB from "../openHAB/openHAB";
 
 const updateColor = (volts, watts) => {
-  let color;
-  if (volts < 25 && watts < 100) {
-    color = "#6aa84f";
-  } else if (volts > 25 && volts < 35 && watts > 100 && watts < 250) {
-    color = "#ef962e";
-  } else {
-    color = "#cc4125";
-  }
-  return { color };
+    let color;
+    if (volts < 25 && watts < 100) {
+        color = "#6aa84f";
+    } else if (volts > 25 && volts < 35 && watts > 100 && watts < 250) {
+        color = "#ef962e";
+    } else {
+        color = "#cc4125";
+    }
+    return {color};
 };
 
 const Overview = () => {
-  let navigate = useNavigate();
+    let navigate = useNavigate();
 
-  const redirectToDevices = () => {
-    navigate("/devices");
-  };
+    const redirectToDevices = () => {
+        navigate("/devices");
+    };
 
-  function redirectToDetailedDevice(id) {
-    let path = generatePath("/devices/:id/details", { id });
-    navigate(path);
-  }
-
-  function redirectToDetailedSwitch(id) {
-    let path = generatePath("/switches/:id/details", { id });
-    navigate(path);
-  }
-
-  let volts = 26;
-  let watts = 101;
-
-  //Fetching openHAB switches
-  const [openHABItems, setOpenHABItems] = useState([]);
-
-  const config = {
-    headers: { Authorization: openHAB.token },
-  };
-
-  useEffect(() => {
-    fetchOpenHABItems();
-  }, []);
-
-  const fetchOpenHABItems = async () => {
-    // https://community.openhab.org/t/cors-problem/113063  --> If requests not working
-    const response = await Axios(openHAB.url + "/rest/items", config);
-    setOpenHABItems(response.data);
-  };
-
-  var switches = [];
-  openHABItems.forEach(function (item) {
-    if (item.type === "Switch") {
-      switches.push(item);
+    function redirectToDetailedDevice(id) {
+        let path = generatePath("/devices/:id/details", {id});
+        navigate(path);
     }
-  });
 
-  console.log(switches);
+    function redirectToDetailedSwitch(id) {
+        let path = generatePath("/switches/:id/details", {id});
+        navigate(path);
+    }
 
-  return (
-    <>
-      <div>
-        <div className="flex-container">
-          <div id="circle" />
-          <div id="horRectangle" />
-          <div id="door" />
-          <div
-            onClick={() => redirectToDetailedDevice(devices_turnedOn[0].title)}
-            style={{ backgroundColor: updateColor(volts, watts).color }}
-            id="washer"
-            className="square"
-          >
+    let volts = 26;
+    let watts = 101;
+
+    //Fetching openHAB switches
+    const [openHABItems, setOpenHABItems] = useState([]);
+
+    const config = {
+        headers: {Authorization: openHAB.token},
+    };
+
+    useEffect(() => {
+        fetchOpenHABItems();
+    }, []);
+
+    const fetchOpenHABItems = async () => {
+        // https://community.openhab.org/t/cors-problem/113063  --> If requests not working
+        const response = await Axios(openHAB.url + "/rest/items", config);
+        setOpenHABItems(response.data);
+    };
+
+    var switches = [];
+    openHABItems.forEach(function (item) {
+        if (item.type === "Switch") {
+            if (item.stateDescription.readOnly === false)
+                switches.push(item);
+        }
+    });
+
+    console.log(switches);
+
+    return (
+        <>
             <div>
-              {volts}W<br />
-              {watts}V
+                <div className="flex-container">
+                    <div id="circle"/>
+                    <div id="horRectangle"/>
+                    <div id="door"/>
+                    <div
+                        onClick={() => redirectToDetailedDevice(devices_turnedOn[0].title)}
+                        style={{backgroundColor: updateColor(volts, watts).color}}
+                        id="washer"
+                        className="square"
+                    >
+                        <div>
+                            {volts}W<br/>
+                            {watts}V
+                        </div>
+                    </div>
+
+                    <div
+                        onClick={() => redirectToDetailedDevice(devices_turnedOn[1].title)}
+                        style={{backgroundColor: updateColor(volts, watts).color}}
+                        id="tv"
+                        className="rectangle"
+                    >
+                        <div>
+                            {volts}W<br/>
+                            {watts}V
+                        </div>
+                    </div>
+                    <div
+                        onClick={() => redirectToDetailedDevice(devices_turnedOn[2].title)}
+                        style={{backgroundColor: updateColor(volts, watts).color}}
+                        id="kitchen"
+                        className="square"
+                    >
+                        <div>
+                            {volts}W<br/>
+                            {watts}V
+                        </div>
+                    </div>
+                    <div
+                        onClick={() => redirectToDetailedDevice(devices_turnedOn[3].title)}
+                        style={{backgroundColor: updateColor(volts, watts).color}}
+                        id="refrigerator"
+                        className="square"
+                    >
+                        <div>
+                            {volts}W<br/>
+                            {watts}V
+                        </div>
+                    </div>
+                    <div
+                        onClick={() => redirectToDetailedDevice(devices_turnedOn[4].title)}
+                        style={{backgroundColor: updateColor(volts, watts).color}}
+                        id="dryer"
+                        className="square"
+                    >
+                        <div>
+                            {volts}W<br/>
+                            {watts}V
+                        </div>
+                    </div>
+
+                    {/* {seen ? <PopUp /> : null} */}
+                </div>
+
+                <div className="header">
+                    <div className="section-header">Turned on</div>
+                    <button
+                        onClick={redirectToDevices}
+                        className="btn-primary-no-background"
+                    >
+                        View all
+                    </button>
+                </div>
+
+                <div className="scroll-area">
+                    {devices_turnedOn.map((src) => (
+                        <button
+                            className="card hov-primary horizontal"
+                            onClick={() => redirectToDetailedDevice(src.title)}
+                        >
+                            <div
+                                key={src.title}
+                                className="card-image horizontal"
+                                style={{
+                                    backgroundImage: `url(${src.image})`,
+                                }}
+                            />
+                            <div className="card-title horizontal">{src.title}</div>
+                        </button>
+                    ))}
+                </div>
+
+                <div className="header">
+                    <div className="section-header">Switches</div>
+                </div>
+                <div className="scroll-area">
+                    {switches.map((src) => (
+                        <button
+                            className="card hov-primary horizontal"
+                            onClick={() => redirectToDetailedSwitch(src.name)}
+                        >
+                            <div
+                                key={src.label}
+                                className="card-image horizontal"
+                                style={{
+                                    backgroundImage: `url(/logo192.png)`,
+                                }}
+                            />
+                            <div className="card-title horizontal">{src.label}</div>
+                        </button>
+                    ))}
+                </div>
+
+                <div className="header">
+                    <div className="section-header">Frequently used</div>
+                    <button
+                        onClick={redirectToDevices}
+                        className="btn-primary-no-background"
+                    >
+                        View all
+                    </button>
+                </div>
+                <div className="scroll-area">
+                    {devices_frequentlyUsed.map((src) => (
+                        <button
+                            className="card hov-primary horizontal"
+                            onClick={() => redirectToDetailedDevice(src.title)}
+                        >
+                            <div
+                                key={src.title}
+                                className="card-image horizontal"
+                                style={{
+                                    backgroundImage: `url(${src.image})`,
+                                }}
+                            />
+                            <div className="card-title horizontal">{src.title}</div>
+                        </button>
+                    ))}
+                </div>
             </div>
-          </div>
-
-          <div
-            onClick={() => redirectToDetailedDevice(devices_turnedOn[1].title)}
-            style={{ backgroundColor: updateColor(volts, watts).color }}
-            id="tv"
-            className="rectangle"
-          >
-            <div>
-              {volts}W<br />
-              {watts}V
-            </div>
-          </div>
-          <div
-            onClick={() => redirectToDetailedDevice(devices_turnedOn[2].title)}
-            style={{ backgroundColor: updateColor(volts, watts).color }}
-            id="kitchen"
-            className="square"
-          >
-            <div>
-              {volts}W<br />
-              {watts}V
-            </div>
-          </div>
-          <div
-            onClick={() => redirectToDetailedDevice(devices_turnedOn[3].title)}
-            style={{ backgroundColor: updateColor(volts, watts).color }}
-            id="refrigerator"
-            className="square"
-          >
-            <div>
-              {volts}W<br />
-              {watts}V
-            </div>
-          </div>
-          <div
-            onClick={() => redirectToDetailedDevice(devices_turnedOn[4].title)}
-            style={{ backgroundColor: updateColor(volts, watts).color }}
-            id="dryer"
-            className="square"
-          >
-            <div>
-              {volts}W<br />
-              {watts}V
-            </div>
-          </div>
-
-          {/* {seen ? <PopUp /> : null} */}
-        </div>
-
-        <div className="header">
-          <div className="section-header">Turned on</div>
-          <button
-            onClick={redirectToDevices}
-            className="btn-primary-no-background"
-          >
-            View all
-          </button>
-        </div>
-
-        <div className="scroll-area">
-          {devices_turnedOn.map((src) => (
-            <button
-              className="card hov-primary horizontal"
-              onClick={() => redirectToDetailedDevice(src.title)}
-            >
-              <div
-                key={src.title}
-                className="card-image horizontal"
-                style={{
-                  backgroundImage: `url(${src.image})`,
-                }}
-              />
-              <div className="card-title horizontal">{src.title}</div>
-            </button>
-          ))}
-        </div>
-
-        <div className="header">
-          <div className="section-header">Switches</div>
-        </div>
-        <div className="scroll-area">
-          {switches.map((src) => (
-            <button
-              className="card hov-primary horizontal"
-              onClick={() => redirectToDetailedSwitch(src.name)}
-            >
-              <div
-                key={src.label}
-                className="card-image horizontal"
-                style={{
-                  backgroundImage: `url(/logo192.png)`,
-                }}
-              />
-              <div className="card-title horizontal">{src.label}</div>
-            </button>
-          ))}
-        </div>
-
-        <div className="header">
-          <div className="section-header">Frequently used</div>
-          <button
-            onClick={redirectToDevices}
-            className="btn-primary-no-background"
-          >
-            View all
-          </button>
-        </div>
-        <div className="scroll-area">
-          {devices_frequentlyUsed.map((src) => (
-            <button
-              className="card hov-primary horizontal"
-              onClick={() => redirectToDetailedDevice(src.title)}
-            >
-              <div
-                key={src.title}
-                className="card-image horizontal"
-                style={{
-                  backgroundImage: `url(${src.image})`,
-                }}
-              />
-              <div className="card-title horizontal">{src.title}</div>
-            </button>
-          ))}
-        </div>
-      </div>
-    </>
-  );
+        </>
+    );
 };
 
 const devices_turnedOn = [
-  { title: "device1", image: "/logo192.png" },
-  { title: "device2", image: "/logo192.png" },
-  { title: "device3", image: "/logo192.png" },
-  { title: "device4", image: "/logo192.png" },
-  { title: "device5", image: "/logo192.png" },
-  { title: "device6", image: "/logo192.png" },
+    {title: "device1", image: "/logo192.png"},
+    {title: "device2", image: "/logo192.png"},
+    {title: "device3", image: "/logo192.png"},
+    {title: "device4", image: "/logo192.png"},
+    {title: "device5", image: "/logo192.png"},
+    {title: "device6", image: "/logo192.png"},
 ];
 
 const devices_frequentlyUsed = [
-  { title: "device7", image: "/logo192.png" },
-  { title: "device8", image: "/logo192.png" },
-  { title: "device9", image: "/logo192.png" },
+    {title: "device7", image: "/logo192.png"},
+    {title: "device8", image: "/logo192.png"},
+    {title: "device9", image: "/logo192.png"},
 ];
 
 export default Overview;
