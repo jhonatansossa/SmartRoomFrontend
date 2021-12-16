d3abe0e919cff8f3e18f8d88faa79f60e1207113
Aleksandr Arshavskiy <aleksandrarshavskiy@gmail.com>
2021-12-16 20:20:35 +0000

    Changed REST request so we get only switches we can control

--- src/Token/openHAB.js
+++
@@ -1,7 +0,0 @@
-const openHAB = {
-  token:
-    "Bearer oh.TestToken2.4Io1Nb0EDvObPz5LoJnOsjS7OD8eUPOyPlfxZKcUkcux3Jy4qePDgnbOT2NM8S3lqe6cZ1Qwp7ZmLOwSKJJw",
-  url: "http://localhost:8080",
-};
-
-module.exports = openHAB;
