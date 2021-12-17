618cbf861be79e19499dbd4656c6b281570ea061
Aleksandr Arshavskiy <aleksandrarshavskiy@gmail.com>
2021-12-17 20:46:08 +0000

    Feature/frontend #44

--- src/screens/DetailedDevice.js
+++ src/screens/DetailedDevice.js
@@ -1,41 +1,33 @@
-import { React } from "react";
-import { useParams } from "react-router-dom";
+import {React} from "react";
+import {useParams} from "react-router-dom";
 import Graphs from "../components/Graphs";
 
 const DetailedDevice = () => {
-  const { id } = useParams();
+    const {id} = useParams();
 
-  const src = {
-    title: "device1",
-    image: "/logo192.png",
-    voltage: 30,
-    power: 240,
-    switchOn: true,
-  };
-
-  return (
-    <div className="vertical-scroll-area">
-      <h2 className="title">{id}</h2>
-      <div className="card vertical">
-        <div
-          key={src.title}
-          className="card-image vertical"
-          style={{
-            backgroundImage: `url(${src.image})`,
-          }}
-        />
-        <div className="card-title card-content">
-          <p>
-            Current voltage: {src.voltage}V<br />
-            Current power: {src.power}W
-          </p>
+    return (
+        <div className="vertical-scroll-area">
+            <h2 className="title">{id}</h2>
+            <div className="card vertical">
+                <div
+                    key={id}
+                    className="card-image vertical"
+                    style={{
+                        backgroundImage: `url('/resources/${id}.png'`,
+                    }}
+                />
+                <div className="card-title card-content">
+                    <p>
+                        Current voltage: 30V<br/>
+                        Current power: 240W
+                    </p>
+                </div>
+            </div>
+            <div className="card vertical">
+                <Graphs/>
+            </div>
         </div>
-      </div>
-      <div className="card vertical">
-        <Graphs />
-      </div>
-    </div>
-  );
+    );
 };
 
 export default DetailedDevice;
