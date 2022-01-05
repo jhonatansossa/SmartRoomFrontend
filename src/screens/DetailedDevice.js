6a314bba6596ef47ad76a9e3b1a9f4f1bde0b531
Ingo Staut <ingo.staut@stud.h-da.de>
2022-01-05 12:21:06 +0100

    Fix: switch button; interval of requests

--- src/screens/DetailedDevice.js
+++ src/screens/DetailedDevice.js
@@ -1,64 +1,73 @@
-import {React, useEffect, useState} from "react";
-import {useNavigate, useParams} from "react-router-dom";
+import { React, useEffect, useState, useRef } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 import Graphs from "../components/Graphs";
 import openHAB from "../openHAB/openHAB";
 import Axios from "axios";
 
 const DetailedDevice = () => {
-    const [openHABItem, setOpenHABItem] = useState();
-    const {id} = useParams();
-    const navigate = useNavigate();
+  const [openHABItem, setOpenHABItem] = useState();
+  const { id } = useParams();
+  const navigate = useNavigate();
 
-    const config = {
-        headers: {
-            Authorization: openHAB.token,
-        },
-    };
+  const config = {
+    headers: {
+      Authorization: openHAB.token,
+    },
+  };
 
-    useEffect(() => {
-        document.title = "SmartRoom – " + id;
-        let auth = sessionStorage.getItem("auth")
-        if(auth !== "true") {
-            navigate("/login");
-        }else{
-            setInterval(function () {
-                fetchOpenHABItem();
-            }, 1000);
-        }
-    }, []);
+  const timerRef = useRef(null);
 
-    const fetchOpenHABItem = async () => {
-        const response = await Axios(openHAB.url + "/rest/items/" + id + "_ACTIVE_EXPORT_KWH", config);
-        setOpenHABItem(response.data);
-    };
-
-    if (openHABItem === undefined) {
-        return <div/>
+  useEffect(() => {
+    document.title = "SmartRoom – " + id;
+    let auth = sessionStorage.getItem("auth");
+    if (auth !== "true") {
+      navigate("/login");
     } else {
-        return (
-            <div className="vertical-scroll-area">
-                <h2 className="title">{openHABItem.stateDescription.options[0].value}</h2>
-                <div className="card vertical">
-                    <div
-                        key={id}
-                        className="card-image vertical"
-                        style={{
-                            backgroundImage: `url('/resources/${id}.svg'`,
-                        }}
-                    />
-                    <div className="card-title card-content">
-                        <p>
-                            Current consumption: <b>{openHABItem.state} kWh</b>
-                        </p>
-                    </div>
-                </div>
-                <div className="card vertical">
-                    <Graphs/>
-                </div>
-            </div>
-        )
+      timerRef.current = setInterval(function () {
+        fetchOpenHABItem();
+      }, 1000);
+      return () => {
+        clearInterval(timerRef.current);
+      };
     }
-}
+  }, []);
 
+  const fetchOpenHABItem = async () => {
+    const response = await Axios(
+      openHAB.url + "/rest/items/" + id + "_ACTIVE_EXPORT_KWH",
+      config
+    );
+    setOpenHABItem(response.data);
+  };
+
+  if (openHABItem === undefined) {
+    return <div />;
+  } else {
+    return (
+      <div className="vertical-scroll-area">
+        <h2 className="title">
+          {openHABItem.stateDescription.options[0].value}
+        </h2>
+        <div className="card vertical">
+          <div
+            key={id}
+            className="card-image vertical"
+            style={{
+              backgroundImage: `url('/resources/${id}.svg'`,
+            }}
+          />
+          <div className="card-title card-content">
+            <p>
+              Current consumption: <b>{openHABItem.state} kWh</b>
+            </p>
+          </div>
+        </div>
+        <div className="card vertical">
+          <Graphs />
+        </div>
+      </div>
+    );
+  }
+};
 
 export default DetailedDevice;
